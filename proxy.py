2930819c6796259d3a26cb05c49860719f5500a5
cribe123 <156344152+cribe123@users.noreply.github.com>
2024-08-22 17:17:43 +0300

    Add files via upload

---
+++ proxy.py
@@ -0,0 +1,4 @@
+import proxyscrape
+collector = proxyscrape.create_collector('my-collector', 'http')
+proxy = collector.get_proxies({'country': 'france'})
+print(f"{proxy.host}:{proxy.port} Анонимность: {proxy.anonymous} ({proxy.country})")
